8afc50855feceb47a147a9d85151300460e5b5ab
Matt Zanchelli <matt@mdznr.com>
2013-09-22 02:59:36 -0400

    Clear background on first app launch. Updated graphics and text to be white. Updated progress bar to be thinner and fit with iOS 7 UI.

--- Albums/MVFirstSyncViewController.m
+++ Albums/MVFirstSyncViewController.m
@@ -68,7 +68,6 @@
 {
 	self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
 	self.view.backgroundColor = [UIColor clearColor];
-	self.view.layer.cornerRadius = 8.0;
 
 	float startY = roundf(self.view.frame.size.height / 4.06);
 
@@ -76,57 +75,58 @@
 	self.iconImageView = [[UIImageView alloc] initWithImage:iconImage];
 	self.iconImageView.frame = CGRectMake(CGRectGetMidX(self.view.frame) -
 										  CGRectGetWidth(self.iconImageView.frame) / 2,
-										  startY,
+										  startY + 10,
 										  self.iconImageView.frame.size.width,
 										  self.iconImageView.frame.size.height);
 	self.iconImageView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
 	                                      UIViewAutoresizingFlexibleRightMargin;
 	[self.view addSubview:self.iconImageView];
 	
-  self.headLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, startY + 70 - 17 / 2,
-                                                             CGRectGetWidth(self.view.frame) - 60,
-                                                             40)];
-  self.headLabel.text = NSLocalizedString(@"Syncing Albums…", @"Syncing Albums Heading");
-  self.headLabel.textColor = [UIColor colorWithRed:0.1961 green:0.1961 blue:0.1961 alpha:1];
-  self.headLabel.textAlignment = NSTextAlignmentCenter;
-  self.headLabel.backgroundColor = [UIColor clearColor];
-  self.headLabel.opaque = NO;
-  self.headLabel.font = [UIFont boldSystemFontOfSize:19];
-  self.headLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
-                                    UIViewAutoresizingFlexibleRightMargin;
-  [self.view addSubview:self.headLabel];
+	self.headLabel = [[UILabel alloc] initWithFrame:CGRectMake(30,
+															   startY + 70 - 17 / 2,
+															   CGRectGetWidth(self.view.frame) - 60,
+															   40)];
+	self.headLabel.text = NSLocalizedString(@"Syncing Albums…", @"Syncing Albums Heading");
+	self.headLabel.textColor = [UIColor colorWithWhite:1.0f alpha:1.0f];
+	self.headLabel.textAlignment = NSTextAlignmentCenter;
+	self.headLabel.backgroundColor = [UIColor clearColor];
+	self.headLabel.opaque = NO;
+	self.headLabel.font = [UIFont boldSystemFontOfSize:19];
+	self.headLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
+									  UIViewAutoresizingFlexibleRightMargin;
+	[self.view addSubview:self.headLabel];
   
-  self.descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(30, startY + 110 - 10.5,
-                                                                    CGRectGetWidth(self.view.frame) - 60,
-                                                                    200)];
-  self.descriptionLabel.text = NSLocalizedString(@"Your Music Library is being browsed to \
-find albums you might enjoy", @"Syncing Description");
-  self.descriptionLabel.textColor = [UIColor colorWithRed:0.4196 green:0.4196 blue:0.4196 alpha:1];
-  self.descriptionLabel.textAlignment = NSTextAlignmentCenter;
-  self.descriptionLabel.backgroundColor = [UIColor clearColor];
-  self.descriptionLabel.opaque = NO;
-  self.descriptionLabel.font = [UIFont systemFontOfSize:15];
-  self.descriptionLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
-                                           UIViewAutoresizingFlexibleRightMargin;
-  self.descriptionLabel.numberOfLines = 0;
-  [self.descriptionLabel sizeToFit];
-  [self.view addSubview:self.descriptionLabel];
-  
-  CGRect progressViewFrame = CGRectMake((CGRectGetWidth(self.view.frame) - 159) / 2,
-                                        CGRectGetMaxY(self.descriptionLabel.frame) + 4 + 8,
-                                        159, 40);
-  self.progressView = [[UIProgressView alloc] initWithFrame:progressViewFrame];
-  
-  UIImage *trackImage = [UIImage imageNamed:@"progress_bar_track"];
-  trackImage = [trackImage resizableImageWithCapInsets:UIEdgeInsetsMake(0, 10, 0, 10)];
-  self.progressView.trackImage = trackImage;
-  
-  UIImage *progressImage = [UIImage imageNamed:@"progress_bar_progress"];
-  progressImage = [progressImage resizableImageWithCapInsets:UIEdgeInsetsMake(0, 10, 0, 10)];
-  self.progressView.progressImage = progressImage;
-  self.progressView.progress = 0.0;
-  self.progressView.frame = progressViewFrame;
-  [self.view addSubview:self.progressView];
+	self.descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(30,
+																	  startY + 110 - 10.5,
+																	  CGRectGetWidth(self.view.frame) - 60,
+																	  200)];
+	self.descriptionLabel.text = NSLocalizedString(@"Your Music Library is being browsed to find albums you might enjoy", @"Syncing Description");
+	self.descriptionLabel.textColor = [UIColor colorWithWhite:1.0f alpha:1.0f];
+	self.descriptionLabel.textAlignment = NSTextAlignmentCenter;
+	self.descriptionLabel.backgroundColor = [UIColor clearColor];
+	self.descriptionLabel.opaque = NO;
+	self.descriptionLabel.font = [UIFont systemFontOfSize:15];
+	self.descriptionLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
+										     UIViewAutoresizingFlexibleRightMargin;
+	self.descriptionLabel.numberOfLines = 0;
+	[self.descriptionLabel sizeToFit];
+	[self.view addSubview:self.descriptionLabel];
+
+	CGRect progressViewFrame = CGRectMake((CGRectGetWidth(self.view.frame) - 159) / 2,
+										  CGRectGetMaxY(self.descriptionLabel.frame) + 4 + 8,
+										  159, 40);
+	self.progressView = [[UIProgressView alloc] initWithFrame:progressViewFrame];
+
+	UIImage *trackImage = [UIImage imageNamed:@"progress_bar_track"];
+	trackImage = [trackImage resizableImageWithCapInsets:UIEdgeInsetsMake(0, 10, 0, 10)];
+	self.progressView.trackImage = trackImage;
+
+	UIImage *progressImage = [UIImage imageNamed:@"progress_bar_progress"];
+	progressImage = [progressImage resizableImageWithCapInsets:UIEdgeInsetsMake(0, 10, 0, 10)];
+	self.progressView.progressImage = progressImage;
+	self.progressView.progress = 0.0;
+	self.progressView.frame = progressViewFrame;
+	[self.view addSubview:self.progressView];
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -138,12 +138,14 @@ find albums you might enjoy", @"Syncing Description");
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object
                         change:(NSDictionary *)change context:(void *)context
 {
-  if(object == self.coreManager)
-  {
-    self.progressView.progress = self.coreManager.progression;
-  }
-  else
-    [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+	if ( object == self.coreManager ) {
+		self.progressView.progress = self.coreManager.progression;
+	} else {
+		[super observeValueForKeyPath:keyPath
+							 ofObject:object
+							   change:change
+							  context:context];
+	}
 }
 
 @end
